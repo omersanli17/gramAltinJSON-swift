7effdefd4f2143a3a56df6d60a46357980c00aa3
Ömer Şanlı <drasonli17@gmail.com>
2021-10-22 23:37:12 +0300

    Add files via upload

---
+++ MadenTurkiyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MadenTurkiyeApp.swift
+//  MadenTurkiye
+//
+//  Created by omer sanli on 22.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MadenTurkiyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
